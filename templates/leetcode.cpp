d2bc9a3ddecc97383d71ba625baa3e5abf124a49
Abdalla Eldoumani <aamsdoumani@gmail.com>
2026-08-21 06:27:43 +0300

    add leetcode template

---
+++ templates/leetcode.cpp
@@ -0,0 +1,19 @@
+// LeetCode starter. LeetCode links this class against its own driver and times
+// the call, so there is no input to parse and nothing for fast I/O to speed up.
+// Structures from codeforces-fast.cpp paste in above the class unchanged; skip
+// custom_hash, which only matters where submissions can be hacked.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x)  (int)(x).size()
+
+class Solution {
+public:
+    int solve(vector<int>& nums) {
+        return sz(nums);
+    }
+};
